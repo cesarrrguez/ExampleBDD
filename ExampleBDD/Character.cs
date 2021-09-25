e97f7d1ac229967ecedfb833765f00667642acd6
César Rodríguez <cesar.rrguez@gmail.com>
2021-09-25 17:47:41 +0200

    Add Character class

---
+++ ExampleBDD/Character.cs
@@ -0,0 +1,16 @@
+﻿namespace ExampleBDD
+{
+    public class Character
+    {
+        public int Health { get; set; }
+        public int Level { get; set; }
+
+        public bool IsAlive => Health > 0;
+
+        public Character()
+        {
+            Health = 1000;
+            Level = 1;
+        }
+    }
+}
