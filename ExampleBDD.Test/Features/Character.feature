b3271ed8442a30d55ebd35e095aa302f42d5753f
César Rodríguez <cesar.rrguez@gmail.com>
2021-09-25 17:49:02 +0200

    Create Scenario "Creation of a character"

---
+++ ExampleBDD.Test/Features/Character.feature
@@ -0,0 +1,7 @@
+﻿Feature: Character
+
+Scenario: Creation of a character
+    Given A new character
+    Then the health starting at 1000
+    And the level starting at 1
+    And starting alive
---
+++ ExampleBDD.Test/Steps/CharacterSteps.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace ExampleBDD.Test.Steps
+{
+    [Binding]
+    public class CharacterSteps
+    {
+        private Character character;
+
+        [Given(@"A new character")]
+        public void GivenANewCharacter()
+        {
+            character = new Character();
+        }
+
+        [Then(@"the health starting at (.*)")]
+        public void ThenTheHealthStartingAt(int health)
+        {
+            Assert.AreEqual(health, character.Health);
+        }
+
+        [Then(@"the level starting at (.*)")]
+        public void ThenTheLevelStartingAt(int level)
+        {
+            Assert.AreEqual(level, character.Level);
+        }
+
+        [Then(@"starting alive")]
+        public void ThenStartingAlive()
+        {
+            Assert.IsTrue(character.IsAlive);
+        }
+    }
+}
