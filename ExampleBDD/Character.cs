a4d00f777f80ebc8518cc8cf650b5cccc52c5499
César Rodríguez <cesar.rrguez@gmail.com>
2021-09-25 17:57:29 +0200

    Add Heal and ReceiveHeal methods to Characer

--- ExampleBDD/Character.cs
+++ ExampleBDD/Character.cs
@@ -18,10 +18,23 @@
             target.ReceiveAttack(attackDamage);
         }
 
+        public void Heal(Character target, int healedValue)
+        {
+            target.ReceiveHeal(healedValue);
+        }
+
         private void ReceiveAttack(int attackDamage)
         {
             Health -= attackDamage;
             if (Health < 0) Health = 0;
+        }
+
+        private void ReceiveHeal(int healValue)
+        {
+            if (!IsAlive) return;
+
+            Health += healValue;
+            if (Health > 1000) Health = 1000;
         }
     }
 }
